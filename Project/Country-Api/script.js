2911e1e48aed9eb557b144821837a8fc99da5754
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-02-27 22:58:40 +0530

    js changed

--- Project/Country-Api/script.js
+++ Project/Country-Api/script.js
@@ -1,6 +1,22 @@
 
-
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+
+const request =new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/name/india');
+request.send();
+request.addEventListener('load',function(){
+
+   const data=JSON.parse(this.responseText)[1];
+   console.log(data);
+
+
+
+
+
+
+
+})
