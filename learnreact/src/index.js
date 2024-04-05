d2063b61f66dd31cbd8e15df4dfad4c4b337edbc
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-05 09:07:27 +0530

    G4 learn react

---
+++ learnreact/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+  <h1>This is our first Page</h1>
+  <p>This is the praragraph</p>
+</>
+);
+
