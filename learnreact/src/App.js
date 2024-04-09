621691236cec5b6ec5f36945c6dd24f2898b0ae0
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-09 09:09:03 +0530

    bootstrap use

---
+++ learnreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function App() {
+  return (
+    <div className=''>
+    
+    <Header/>
+    </div>
+  )
+}
--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
-  <h1>This is our first Page</h1>
-  <p>This is the praragraph</p>
+  <App/>
 </>
 );
 
