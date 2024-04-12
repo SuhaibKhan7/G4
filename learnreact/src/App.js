fe1cd98ea0fdcc75b32da99f21dbc86144256b6e
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-12 20:26:13 +0530

    G4 USEEFFECT

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       {/* <Header />
       <TopSection />
       <CardData /> */}
-      {/* <LearnUseEffect/> */}
+      <LearnUseEffect/>
 
     </div>
   )
--- learnreact/src/components/LearnUseEffect.js
+++ learnreact/src/components/LearnUseEffect.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
 
 export default function LearnUseEffect() {
-const [counter,setCounter]=useState(0)
-useEffect( ()=>{
+  const [counter, setCounter] = useState(0)
+  // useEffect(() => {
 
-console.log('Mount')
-}, [counter])
+  //   console.log('Mount'+ counter)
 
+
+  //   return () => {
+  //     console.log("unmount"+ counter)
+  //   }
+
+  // },[counter])
+ 
+  let [cardata, setcarddata] = useState([])
+  fetch('http://localhost:3004/products')
+    .then((res) => res.json())
+    .then((data) => setcarddata(data)
+    )
 
   return (
     <div>
-
-<h1>counter: {counter}</h1>
-<button className='btn btn-danger' onClick={()=>setCounter(counter+1)}>Inc</button>
-
+      <h1>counter: {counter}</h1>
+      <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>Inc</button>
+      <h1>printing JSON File</h1>
     </div>
   )
 }
