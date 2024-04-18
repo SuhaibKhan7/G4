2eee87ac375fb0c686e7e5d48be53e5b5b1488d5
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-18 09:08:39 +0530

    CUSTOM HOOK

--- learnreact/src/Hooks/useFetch.js
+++ learnreact/src/Hooks/useFetch.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 
-export  function useFetch(url) {
-    const [data,setData]=useState(null);
-    const [loading,setLoading]=useState(false)
-    useEffect(()=>{
-          const fetchdata=async()=> {
+export function useFetch(url) {
+
+ const[data,setData]=useState(null);
+  const [loading,setLoading]=useState(false)
+      useEffect(() => {
+          async function getdata() {
             setLoading(true);
-            const response = await fetch(url);
-            console.log(response);
-            const result= await response.json();
-            console.log(data);
-            setLoading(false);
-            setData(result);
-        }
-        fetchdata();
-    },[url])
-  return {data,loading}
+              let response = await fetch(url)
+              let result = await response.json();
+              setData(result)
+              setLoading(false);
+          }
+          getdata();
+      },[url])
+    console.log(data);
+      return {data,loading}
 }
--- learnreact/src/components/LearnUseEffect.js
+++ learnreact/src/components/LearnUseEffect.js
@@ -2,33 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { useFetch } from '../Hooks/useFetch'
 export default function LearnUseEffect() {
   const [counter, setCounter] = useState(0)
-  // let card;
-  // fetch('http://localhost:3004/products')
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     card = data
-  //     console.log("card "+card)
-  //   }) //not a valid method _ no sync established
-  const [card, setCard] = useState([]);
-  const [url, setUrl] = useState('http://localhost:3004/products')
-  useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => setCard(data))
-  },[url])
-
+  let [url, setUrl] = useState('http://localhost:3004/products')
+  let { data:card, loading: loading } = useFetch(url)
+  console.log(loading);
 
   return (
     <div>
       <h1>counter: {counter}</h1>
       <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>Inc</button>
       <h1>printing JSON File</h1>
+      {loading && <p>Data is loading</p>}
       <button className='bg-primary' onClick={() => setUrl('http://localhost:3004/products?instock=true')} >Instock</button>
       <button className='bg-danger' onClick={() => setUrl('http://localhost:3004/products?instock=false')}>OutOfstock</button>
       <div className='bg-success text-white p-3 ' >
-        {card.map((c)=>
-        {
-          return <li key={c.id}>{c.id}----{c.pname}---{c.Price}---<span className={JSON.parse(c.instock)?'bg-primary':'bg-danger'}> {JSON.parse(c.instock)?"Instock":"Outofstock"}</span></li>
+        {card && card.map((c) => {
+          return <li key={c.id}>{c.id}----{c.pname}---{c.Price}---<span className={JSON.parse(c.instock) ? 'bg-primary' : 'bg-danger'}> {JSON.parse(c.instock) ? "Instock" : "Outofstock"}</span></li>
         })}
       </div>
     </div>
