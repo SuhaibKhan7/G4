756541a09587fb4b5e27cd8249ea296fd794354d
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-18 15:59:33 +0530

    try catch use effect

--- learnreact/src/Hooks/useFetch.js
+++ learnreact/src/Hooks/useFetch.js
@@ -1,24 +1,35 @@
-import React, {useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export function useFetch(url) {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    useEffect(() => {
 
- const[data,setData]=useState(null);
-  const [loading,setLoading]=useState(false);
-      useEffect(() => {
-        const fetchdata=async ()=>
-        {
-            setLoading(true); 
-              let response = await fetch(url)
-              let result = await response.json();
-              {setData(result)
-              setLoading(false);
-              }
-          }
-          fetchdata();
-      },[url])
+        const getdata = async () => {
+            try {
+                let response = await fetch(url)
+                console.log(response)
+                if (!response.ok) {
+                    throw new Error(`Failed to Fetch Data  ${response.status}`)
+                }
+                let result = await response.json()
+                {
+                    setData(result);
+                }
+            }
+            catch (err) {
+                setError(err.message)
+
+
+            }
 
 
 
-    console.log(data);
-      return {data,loading}
+        }
+
+
+        getdata();
+    }, [])
+
+    return {data,error};
 }
--- learnreact/src/components/LearnUseEffect.js
+++ learnreact/src/components/LearnUseEffect.js
@@ -1,25 +1,18 @@
 import React, { useEffect, useState } from 'react'
-
+import { useFetch } from '../Hooks/useFetch'
 export default function LearnUseEffect() {
 
   const [counter, setCounter] = useState(0)
-  let [url, setUrl] = useState('http://localhost:3004/products')
-  const [data, setData] = useState(null);
-
- useEffect(()=>{
-  
-   fetch(url)
-   .then((response)=>response.json())
-   .then((result)=>setData(result))
-
- },[])
-
+  let [url, setUrl] = useState('http://localhost:3004/product')
+  let {data,error} = useFetch(url)
   return (
     <div>
       <h1>counter: {counter}</h1>
       <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>Inc</button>
       <h1>printing JSON File</h1>
-     
+
+      <h1>{error}</h1>
+
       <button className='bg-primary' onClick={() => setUrl('http://localhost:3004/products?instock=true')} >Instock</button>
       <button className='bg-danger' onClick={() => setUrl('http://localhost:3004/products?instock=false')}>OutOfstock</button>
       <div className='bg-success text-white p-3 ' >
