65d23a78ae5f2a521a3cb53616a879ae0ccd27b1
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-05-15 00:43:24 +0530

    Update index.js

--- Last/backend/index.js
+++ Last/backend/index.js
@@ -8,10 +8,13 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/learn');
+    await mongoose.connect('mongodb+srv://singup:oezvKnvAszgic2R6@cluster67376.kmci2em.mongodb.net/');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+
+
 const Userschema = new mongoose.Schema({
     name: String
 });
